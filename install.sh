8cd0263eceae5f4e57248aa67a96d52ed0d81a2e
Xiwei Xuan <xwxuan@ucdavis.edu>
2025-10-15 11:34:56 -0700

    Installation and Data preparation

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install --upgrade pip
+pip install dash pandas matplotlib nbformat PyYAML tqdm Werkzeug comm ipython wordcloud
+pip3 install -U scikit-learn
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
+pip install pycocotools
+pip install --upgrade openai
+pip install opencv-python
+pip install easydict transformers ftfy
